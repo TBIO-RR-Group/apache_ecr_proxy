311a5b7f7738fdd88352d945655a219fdbb377a7
EC2 Default User <ec2-user@ip-172-25-130-232.rdcloud.bms.com>
2022-04-06 02:09:10 +0000

    Initial checkin of entrypoint script that basicallyc ombines auth_init.sh and auth_update.sh

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+
+#See here for where I got the escape subs: https://unix.stackexchange.com/questions/598461/how-to-espace-sequence-of-characters-in-sed
+#Also see these:
+#https://stackoverflow.com/questions/29613304/is-it-possible-to-escape-regex-metacharacters-reliably-with-sed
+#https://stackoverflow.com/questions/2854655/command-to-escape-a-string-in-bash
+#https://stackoverflow.com/questions/7680504/sed-substitution-with-bash-variables
+
+AUTH_INFO=$(aws ecr get-authorization-token --output text)
+ECR_TOKEN=$(echo "$AUTH_INFO" | cut -f2)
+ECR_TOKEN_EXPIRES=$(echo "$AUTH_INFO" | cut -f3)
+ECR_TOKEN_ENDPOINT=$(echo "$AUTH_INFO" | cut -f4)
+ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'\'/'\\'}
+ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'/'/'\/'}
+ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//':'/'\:'}
+ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'.'/'\.'}
+ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'-'/'\-'}
+SED_CMD="sed -i s/__ECR_ENDPOINT__/$ECR_TOKEN_ENDPOINT/g /etc/apache2/sites-enabled/default-ssl.conf"
+$($SED_CMD)
+ECR_TOKEN=$ECR_TOKEN eval 'apachectl -k start'
+
+while true
+do
+    sleep 6h
+    AUTH_INFO=$(aws ecr get-authorization-token --output text)
+    ECR_TOKEN=$(echo "$AUTH_INFO" | cut -f2)
+    ECR_TOKEN_EXPIRES=$(echo "$AUTH_INFO" | cut -f3)
+    ECR_TOKEN_ENDPOINT=$(echo "$AUTH_INFO" | cut -f4)
+    #Restart Apache with new token:
+    ECR_TOKEN=$ECR_TOKEN eval 'apachectl -k graceful'
+done
