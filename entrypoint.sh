7961baf61f52067af0cdf0a3f2ff286597f3b9b6
EC2 Default User <ec2-user@ip-172-25-130-232.rdcloud.bms.com>
2022-10-09 15:33:55 +0000

    Modified to just fully replace default-ssl.conf (from TEMPLATE VERSION), then sed in specific values, then either start (first time) or restart graceful (in single loop)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,26 +6,29 @@
 #https://stackoverflow.com/questions/2854655/command-to-escape-a-string-in-bash
 #https://stackoverflow.com/questions/7680504/sed-substitution-with-bash-variables
 
-AUTH_INFO=$(aws ecr get-authorization-token --output text)
-ECR_TOKEN=$(echo "$AUTH_INFO" | cut -f2)
-ECR_TOKEN_EXPIRES=$(echo "$AUTH_INFO" | cut -f3)
-ECR_TOKEN_ENDPOINT=$(echo "$AUTH_INFO" | cut -f4)
-ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'\'/'\\'}
-ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'/'/'\/'}
-ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//':'/'\:'}
-ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'.'/'\.'}
-ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'-'/'\-'}
-SED_CMD="sed -i s/__ECR_ENDPOINT__/$ECR_TOKEN_ENDPOINT/g /etc/apache2/sites-available/default-ssl.conf"
-$($SED_CMD)
-ECR_TOKEN=$ECR_TOKEN eval 'apachectl -k start'
+INIT_DONE=""
 
 while true
 do
-    sleep 6h
     AUTH_INFO=$(aws ecr get-authorization-token --output text)
     ECR_TOKEN=$(echo "$AUTH_INFO" | cut -f2)
     ECR_TOKEN_EXPIRES=$(echo "$AUTH_INFO" | cut -f3)
     ECR_TOKEN_ENDPOINT=$(echo "$AUTH_INFO" | cut -f4)
-    #Restart Apache with new token:
-    ECR_TOKEN=$ECR_TOKEN eval 'apachectl -k graceful'
+    ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'\'/'\\'}
+    ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'/'/'\/'}
+    ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//':'/'\:'}
+    ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'.'/'\.'}
+    ECR_TOKEN_ENDPOINT=${ECR_TOKEN_ENDPOINT//'-'/'\-'}
+    cp /etc/apache2/sites-available/default-ssl.confTEMPLATE /etc/apache2/sites-available/default-ssl.conf
+    SED_CMD="sed -i s/__ECR_ENDPOINT__/$ECR_TOKEN_ENDPOINT/g /etc/apache2/sites-available/default-ssl.conf"
+    $($SED_CMD)
+    SED_CMD2="sed -i s/__ECR_TOKEN__/$ECR_TOKEN/g /etc/apache2/sites-available/default-ssl.conf"
+    $($SED_CMD2)
+    if [ -z $INIT_DONE ]; then
+	INIT_DONE="TRUE"
+	apachectl -k start
+    else
+	apachectl -k graceful
+    fi
+    sleep 6h
 done
