c08297270472fd23a47740096c2b51adf8aaa586
EC2 Default User <ec2-user@ip-172-25-130-232.rdcloud.bms.com>
2022-10-09 15:28:55 +0000

    Removing 2 files that aren't actually needed (their functionality is encapsulated in entrypoint.sh)

--- auth_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -l
-
-AUTH_INFO=$(aws ecr get-authorization-token --output text)
-ECR_TOKEN=$(echo "$AUTH_INFO" | cut -f2)
-ECR_TOKEN_EXPIRES=$(echo "$AUTH_INFO" | cut -f3)
-ECR_TOKEN_ENDPOINT=$(echo "$AUTH_INFO" | cut -f4)
-#Restart Apache with new token:
-ECR_TOKEN=$ECR_TOKEN eval 'apachectl -k graceful'
